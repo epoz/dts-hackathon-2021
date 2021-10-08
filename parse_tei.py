e45b167dcfa8e4f33b80b7f4d3b84af16dc6ee02
Etienne Posthumus <eposthumus@gmail.com>
2021-10-08 17:04:39 +0200

    Get basic levels

--- parse_tei.py
+++ parse_tei.py
@@ -1,3 +1,4 @@
+import json
 import zipfile
 import lxml, lxml.etree
 from rich.progress import track
@@ -11,14 +12,13 @@ for file_info in track(z.infolist()):
     file_contents = z.read(file_info)
     try:
         doc = lxml.etree.fromstring(file_contents, parser)
-        divs = doc.xpath(".//div")
-        div_chapters = doc.xpath(".//div[@type='chapter']")
-        div_sections = doc.xpath(".//div[@type='section']")
-        DATA[file_info.filename] = (
-            doc,
-            len(divs),
-            len(div_chapters),
-            len(div_sections),
-        )
+        levels = []
+        for i_d1, d1 in enumerate(doc.xpath("text/body/div")):
+            levels.append(str(i_d1 + 1))
+            for i_d2, d2 in enumerate(d1.xpath("div")):
+                levels.append(f"{i_d1+1}.{i_d2+1}")
+        DATA[file_info.filename] = levels
     except lxml.etree.XMLSyntaxError:
         ISSUES.append(file_info.filename)
+
+open("tei_files.json", "w").write(json.dumps(DATA, indent=2))
